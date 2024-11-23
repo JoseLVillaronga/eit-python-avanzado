1c11858dedc580e4e845b58ae3c34ae6e4c9bcce
José Luis Villaronga <jlvillaronga@teccam.net>
2024-11-23 11:46:50 -0300

    Update

---
+++ Clase1/3_ejercicio.py
@@ -0,0 +1,6 @@
+"""
+Consigna: mostrar por la terminal el mensaje:
+"La función upper() sirve para poner en mayúsculas las cadenas"
+La subcadena 'upper()' debe ser guardada en una variable.
+Para el primer print, usar las 3 formas concatenación, +, coma, f-strings
+"""
